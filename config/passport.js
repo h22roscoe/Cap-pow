4eb85ac843f3643093ee5a2b711d0c199a641191
Harry <harry.roscoe14@imperial.ac.uk>
2016-05-30 00:34:19 +0100

    added the login methods so that signed up users can log in.

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ var LocalStrategy = require('passport-local').Strategy;
 // load up the user model
 var User = require('../app/models/user');
 
-function createNewUser() {
+function createNewUser(email, password, done) {
     // If there is no user with that email create the user
     var newUser = new User();
 
@@ -22,7 +22,7 @@ function createNewUser() {
     });
 }
 
-function findUser() {
+function findUser(email, password, done) {
     // Find a user whose email is the same as the forms email
     // we are checking to see if the user trying to login
     // already exists
@@ -37,9 +37,10 @@ function findUser() {
         // Check to see if theres already a user with that email
         if (user) {
             return done(null, false,
-                req.flash("signupMessage", "That email is already taken."));
+                req.flash("signupMessage",
+                    "That email is already taken."));
         } else {
-            createNewUser();
+            createNewUser(email, password, done);
         }
     });
 }
@@ -52,16 +53,13 @@ module.exports = function (passport) {
 
     // Used to serialize the user for the session
     passport.serializeUser(function (user, done) {
-        console.log(user.id + " was seralized");
         done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function (id, done) {
-        User.findOne({ where: {id: id} }).then(function(user) {
-            done(null, user);
-        }).catch(function(error) {
-            done(error);
+        User.findById(id, function(err, user) {
+            done(err, user);
         });
     });
 
@@ -80,6 +78,49 @@ module.exports = function (passport) {
         }, function (req, email, password, done) {
             // Asynchronous
             // User.findOne wont fire unless data is sent back
-            process.nextTick(findUser);
-        }));
+            process.nextTick(findUser.bind(null, email, password, done));
+        })
+    );
+
+    passport.use('local-login',
+        new LocalStrategy({
+            // By default, local strategy uses username and password, we will
+            // override with email
+            usernameField: 'email',
+            passwordField: 'password',
+
+            // allows us to pass back the entire request to the callback
+            passReqToCallback: true
+        }, function (req, email, password, done) {
+            // Callback with email and password from our form
+
+            // Find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            User.findOne({
+                'local.email': email
+            }, function (err, user) {
+                // if there are any errors, return the error before anything else
+                if (err) {
+                    return done(err);
+                }
+
+                // if no user is found, return the message
+                if (!user) {
+                    return done(null, false, req.flash('loginMessage',
+                                                       'No user found.'));
+                }
+
+                // if the user is found but the password is wrong
+                if (!user.validPassword(password)) {
+                    return done(null, false,
+                                req.flash('loginMessage',
+                                          'Oops! Wrong password.'));
+                }
+
+                // all is well, return successful user
+                return done(null, user);
+            });
+
+        })
+    );
 };
