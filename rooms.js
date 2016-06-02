4baae79254c02dda8ce298c1f75a36dea13eb1ba
kp1314 <kp1314@ic.ac.uk>
2016-06-02 14:57:04 +0100

    made server side changes to rooms.js

--- rooms.js
+++ rooms.js
@@ -49,24 +49,6 @@ function leaveRoom(data) {
     HOST ACTIONS
 */
 
-// Host creates a new room which people can join
-// Called when we have checked the room name doesn't exist.
-function createNewRoom(data) {
-    // Create a unique Socket.IO Room
-
-    // Return the Room ID (gameId)
-    // to the browser client
-    this.emit("joinedRoom", {
-        //The HTML will check that the roomName entered is unique, we don't worry about that here
-        roomName: data.roomName,
-    });
-
-    // Creates room, joins the room and wait for the players
-    this.join(data.roomName);
-
-    // Maybe render game waiting screen for the host
-}
-
 // Called when there are between 2 and 4 players present.
 // Stops people being able to connect to this room as it
 // is removed from the list of available rooms.
@@ -85,8 +67,8 @@ function startCountDown(data) {
     };
 
 
-        // Will create quintus engine for each player and render their screen
-        // to the game screen html or put a start game button which links to game
+    // Will create quintus engine for each player and render their screen
+    // to the game screen html or put a start game button which links to game
 }
 
 module.exports = function (roomio, roomSocket) {
@@ -97,7 +79,7 @@ module.exports = function (roomio, roomSocket) {
     //Emitted when the create button is pressed, will call this function and then redirect to 'in lobby' page
     roomSocket.on("createNewRoom", createNewRoom);
     //Emitted when start button pressed (this only shows to host when they are in lobby), calls function and then redirects to game
-    roomSocket.on("countDownNow", startCountDown);
+    roomSocket.on("countDown", startCountDown);
 
     // Player Events
     roomSocket.on("joinRoom", joinRoom);
