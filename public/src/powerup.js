c84590e257a898135b9c186076d49bd2da6b4d1d
Billy Springsteen <william.springsteen14@imperial.ac.uk>
2016-06-08 14:03:04 +0100

    Moved all components into powerup.js, also added a line of code in each component to stop each player calling the step function of a component after the component is deleted (if this was necessary - it was more of a precaution)

--- public/src/powerup.js
+++ public/src/powerup.js
@@ -43,6 +43,38 @@ Quintus.Powerup = function (Q) {
         }
     });
 
+    Q.component("slow", {
+
+        //this.entity is the player holding the component (do this.entity.p.speed to access the player's speed, etc)
+
+        //Called when player.add("slow") happens
+        added: function () {
+            // Slow the player down to half speed as soon as component is added
+            // Doing this, rather than set speed to 50, allows us to stack components on a player
+            this.entity.p.speed = this.entity.p.speed / 2;
+            //Will last for 5 seconds (60 frames per second)
+            this.timeLeft = 5 * 60;
+            //Whenever the entity steps, this component's step function will be called too
+            this.entity.on("step", this, "step");
+        },
+
+        step: function (dt) {
+            //If the player has had this component for long enough
+            if (this.timeLeft == 0) {
+                //Double the players speed back to what it would have been without this powerup
+                this.entity.p.speed = this.entity.p.speed * 2;
+                this.entity.off("step", this, "step");
+                this.entity.p.del("slow");
+            } else {
+                timeLeft--;
+            }
+        }
+
+        //Can add any other functions here that you might want to player.slow.function() on (Gun example is refillAmmo())
+
+        //Can extend the player to have some more functions while this component is equipped (Gun example is fire())
+    });
+
     Q.Powerup.extend("Fast", {
         init: function (p) {
             this._super(p, {
@@ -56,6 +88,27 @@ Quintus.Powerup = function (Q) {
             });
             this.destroy();
         }
+    });
+
+    Q.component("fast", {
+
+        added: function () {
+            // Double the players speed
+            this.entity.p.speed = this.entity.p.speed * 2;
+            this.timeLeft = 5 * 60;
+            this.entity.on("step", this, "step");
+        },
+
+        step: function (dt) {
+            if (this.timeLeft == 0) {
+                this.entity.p.speed = this.entity.p.speed / 2;
+                this.entity.off("step", this, "step");
+                this.entity.p.del("fast");
+            } else {
+                timeLeft--;
+            }
+        }
+
     });
 
     Q.Powerup.extend("Heavy", {
@@ -73,6 +126,27 @@ Quintus.Powerup = function (Q) {
         }
     });
 
+    Q.component("heavy", {
+
+        added: function () {
+            //Double the player's gravity
+            this.entity.p.gravity = this.entity.p.gravity * 2;
+            this.timeLeft = 5 * 60;
+            this.entity.on("step", this, "step");
+        },
+
+        step: function (dt) {
+            if (this.timeLeft == 0) {
+                this.entity.p.gravity = this.entity.p.gravity / 2;
+                this.entity.off("step", this, "step");
+                this.entity.p.del("heavy");
+            } else {
+                timeLeft--;
+            }
+        }
+
+    });
+
     Q.Powerup.extend("Light", {
         init: function (p) {
             this._super(p, {
@@ -88,6 +162,27 @@ Quintus.Powerup = function (Q) {
         }
     });
 
+    Q.component("light", {
+
+        added: function () {
+            //Double the player's gravity
+            this.entity.p.gravity = this.entity.p.gravity / 2;
+            this.timeLeft = 5 * 60;
+            this.entity.on("step", this, "step");
+        },
+
+        step: function (dt) {
+            if (this.timeLeft == 0) {
+                this.entity.p.gravity = this.entity.p.gravity * 2;
+                this.entity.off("step", this, "step");
+                this.entity.p.del("light");
+            } else {
+                timeLeft--;
+            }
+        }
+
+    });
+
     Q.Powerup.extend("Freeze", {
         init: function (p) {
             this._super(p, {
@@ -101,6 +196,45 @@ Quintus.Powerup = function (Q) {
             });
             this.destroy();
         }
+    });
+
+    Q.component("freeze", {
+
+        added: function () {
+            //Make velocity of player 0 as soon as they receive powerup
+            p.vx = 0;
+            p.vy = 0;
+            this.timeLeft = 5 * 60;
+            this.entity.on("step", this, "step");
+        },
+
+        step: function (dt) {
+
+            //If the player tries to move, set it's velocity to 0
+
+            this.entity.p.direction = Q.inputs['left'] ? 'left' :
+                Q.inputs['right'] ? 'right' :
+                Q.inputs['up'] ? 'up' :
+                Q.inputs['down'] ? 'down' : this.entity.p.direction;
+
+            switch (this.entity.p.direction) {
+                case "left":
+                case "right":
+                case "up":
+                case "down":
+                    p.vx = 0;
+                    p.vy = 0;
+                    break;
+
+            }
+            if (this.timeLeft == 0) {
+                this.entity.off("step", this, "step");
+                this.entity.p.del("freeze");
+            } else {
+                timeLeft--;
+            }
+        }
+
     });
 
 }
