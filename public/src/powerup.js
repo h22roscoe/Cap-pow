10054b0e82d490276beafb5958c8fa84fc9302da
Billy Springsteen <william.springsteen14@imperial.ac.uk>
2016-06-09 13:11:02 +0100

    Changed how the slow powerup works, so we don't get negative speed

--- public/src/powerup.js
+++ public/src/powerup.js
@@ -33,7 +33,7 @@ Quintus.Powerup = function (Q) {
         added: function () {
             // Slow the player down to half speed as soon as component is added
             // Doing this, rather than set speed to 50, allows us to stack components on a player
-            this.entity.p.speed -= 300;
+            this.entity.p.speed = this.entity.p.speed / 2;
             //Will last for 5 seconds (60 frames per second)
             this.timeLeft = 10 * 60;
             //Whenever the entity steps, this component's step function will be called too
@@ -44,7 +44,7 @@ Quintus.Powerup = function (Q) {
             //If the player has had this component for long enough
             if (this.timeLeft == 0) {
                 //Double the players speed back to what it would have been without this powerup
-                this.entity.p.speed += 300;
+                this.entity.p.speed = this.entity.p.speed * 2;
                 // this.entity.off("step", this, "step");
                 this.entity.del("slow");
             } else {
@@ -255,5 +255,27 @@ Quintus.Powerup = function (Q) {
         }
 
     });
+    
+  /*  Q.Sprite.extend("FlagMove", {
+        init: function (p) {
+            this._super(p, {
+                sheet: "White",
+                type: Q.SPRITE_POWERUP,
+                collisionMask: Q.SPRITE_PLAYER,
+                sensor: true,
+                gravity: 0
+            });
+
+            this.on("sensor");
+        },
+
+        sensor: function (colObj) {
+            colObj.p.socket.broadcast.to(colObj.p.roomName).emit("flag", {
+                playerId: colObj.p.playerId,
+                
+            });
+            this.destroy();
+        }
+    });  */
 
 }
