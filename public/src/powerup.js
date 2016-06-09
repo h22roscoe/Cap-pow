85dd820608138b9d047a22e93e6bdde1b70e8564
h22roscoe <harry.roscoe14@imperial.ac.uk>
2016-06-09 13:19:54 +0100

    Cleaned up a bit

--- public/src/powerup.js
+++ public/src/powerup.js
@@ -1,9 +1,6 @@
-// Put all code in here except what happens to players when it is collected
 Quintus.Powerup = function (Q) {
-
     Q.Sprite.extend("Slow", {
         init: function (p) {
-            //Concrete implementation of a Q.Powerup
             this._super(p, {
                 sheet: "red",
                 type: Q.SPRITE_POWERUP,
@@ -11,32 +8,31 @@ Quintus.Powerup = function (Q) {
                 sensor: true,
                 gravity: 0
             });
-            //Call sensor() when powerup is hit
             this.on("sensor");
         },
 
-        //When a slow powerup is hit (Called instead of sensor() in Powerup)
+        // When a slow powerup is hit (Called instead of sensor() in Powerup)
         sensor: function (colObj) {
-            //Tell every other player in the room that they should equip a slow component, emitting the id of
-            //the player who collided with the powerup, so that we can  tell the other players who activated the powerup
+            // Tell every other player in the room that they should equip a slow component, emitting the id of
+            // the player who collided with the powerup, so that we can  tell the other players who activated the powerup
             colObj.p.socket.broadcast.to(colObj.p.roomName).emit("slow", {
                 playerId: colObj.p.playerId
             });
-            //Destroy the powerup sprite as it has been collected
+            // Destroy the powerup sprite as it has been collected
             this.destroy();
         }
     });
 
     Q.component("slow", {
-        //this.entity is the player holding the component (do this.entity.p.speed to access the player's speed, etc)
-        //Called when player.add("slow") happens
+        // this.entity is the player holding the component (do this.entity.p.speed to access the player's speed, etc)
+        // Called when player.add("slow") happens
         added: function () {
             // Slow the player down to half speed as soon as component is added
             // Doing this, rather than set speed to 50, allows us to stack components on a player
             this.entity.p.speed = this.entity.p.speed / 2;
-            //Will last for 5 seconds (60 frames per second)
-            this.timeLeft = 10 * 60;
-            //Whenever the entity steps, this component's step function will be called too
+            // Will last for 5 seconds (60 frames per second)
+            this.timeLeft = 5 * 60;
+            // Whenever the entity steps, this component's step function will be called too
             this.entity.on("step", this, "step");
         },
 
@@ -46,15 +42,11 @@ Quintus.Powerup = function (Q) {
                 //Double the players speed back to what it would have been without this powerup
                 this.entity.p.speed = this.entity.p.speed * 2;
                 // this.entity.off("step", this, "step");
-                this.entity.del("slow");
+                this.entity.p.del("slow");
             } else {
                 this.timeLeft--;
             }
         }
-
-        //Can add any other functions here that you might want to player.slow.function() on (Gun example is refillAmmo())
-
-        //Can extend the player to have some more functions while this component is equipped (Gun example is fire())
     });
 
     Q.Sprite.extend("Fast", {
@@ -68,22 +60,10 @@ Quintus.Powerup = function (Q) {
             });
             console.log("Binding sensor");
             this.on("sensor");
-            /*this.on("hit.sprite", function (collision) {
-                console.log("HIT");
-                if (collision.obj.isA("Player")) {
-                    collision.obj.p.socket.broadcast.to(colObj.p.roomName).emit("fast", {
-                        playerId: colObj.p.playerId
-                    });
-                    this.destroy();
-                }
-            });*/
             this.add("2d");
         },
 
         sensor: function (colObj) {
-            //console.log(colObj.p);
-            //console.log(colObj.p.socket.broadcast);
-            //colObj.p.socket.broadcast.to(colObj.p.roomName).emit("fast", {
             colObj.p.socket.emit("fast", {
                 playerId: colObj.p.playerId
             });
@@ -92,24 +72,22 @@ Quintus.Powerup = function (Q) {
     });
 
     Q.component("fast", {
-
         added: function () {
             // Double the players speed
-            this.entity.p.speed += 300;
-            this.timeLeft = 10 * 60;
+            this.entity.p.speed = this.entity.p.speed * 2;
+            this.timeLeft = 5 * 60;
             this.entity.on("step", this, "step");
         },
 
         step: function (dt) {
             if (this.timeLeft == 0) {
-                this.entity.p.speed -= 300;
+                this.entity.p.speed = this.entity.p.speed / 2;
                 // this.entity.off("step", this, "step");
-                this.entity.del("fast");
+                this.entity.p.del("fast");
             } else {
                 this.timeLeft--;
             }
         }
-
     });
 
     Q.Sprite.extend("Heavy", {
@@ -146,7 +124,7 @@ Quintus.Powerup = function (Q) {
             if (this.timeLeft == 0) {
                 this.entity.p.gravity = this.entity.p.gravity / 2;
                 // this.entity.off("step", this, "step");
-                this.entity.del("heavy");
+                this.entity.p.del("heavy");
             } else {
                 this.timeLeft--;
             }
@@ -188,7 +166,7 @@ Quintus.Powerup = function (Q) {
             if (this.timeLeft == 0) {
                 this.entity.p.gravity = this.entity.p.gravity * 2;
                 // this.entity.off("step", this, "step");
-                this.entity.del("light");
+                this.entity.p.del("light");
             } else {
                 this.timeLeft--;
             }
@@ -248,34 +226,12 @@ Quintus.Powerup = function (Q) {
             }
             if (this.timeLeft == 0) {
                 // this.entity.off("step", this, "step");
-                this.entity.del("freeze");
+                this.entity.p.del("freeze");
             } else {
-                this.timeLeft--;
+                thistimeLeft--;
             }
         }
 
     });
-    
-  /*  Q.Sprite.extend("FlagMove", {
-        init: function (p) {
-            this._super(p, {
-                sheet: "White",
-                type: Q.SPRITE_POWERUP,
-                collisionMask: Q.SPRITE_PLAYER,
-                sensor: true,
-                gravity: 0
-            });
-
-            this.on("sensor");
-        },
-
-        sensor: function (colObj) {
-            colObj.p.socket.broadcast.to(colObj.p.roomName).emit("flag", {
-                playerId: colObj.p.playerId,
-                
-            });
-            this.destroy();
-        }
-    });  */
 
 }
