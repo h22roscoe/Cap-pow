b40a00182a569fb379f6b9b0df0d8728bad36596
h22roscoe <harry.roscoe14@imperial.ac.uk>
2016-06-12 13:44:16 +0100

    Added the end game horns noise.

--- public/src/game.js
+++ public/src/game.js
@@ -59,7 +59,8 @@ var files = [
     "../data/sprites.json",
     "../images/sprites.png",
     "../images/powerups.png",
-    "../data/powerups.json"
+    "../data/powerups.json",
+    "../audio/MLGHornsSoundEffect.mp3"
 ];
 
 var roomName = sessionStorage.getItem("roomName");
@@ -92,21 +93,23 @@ Q.scene("endGame", function(stage) {
     var box = stage.insert(new Q.UI.Container({
         x: Q.width / 2,
         y: Q.height / 2,
-        fill: "rgba(0,0,0,0.5)"
+        fill: "rgba(255,255,255,0.5)"
     }));
 
     var button = box.insert(new Q.UI.Button({
         x: 0,
         y: 0,
-        fill: "#CCCCCC",
+        fill: "#888888",
         label: "Back to room"
-    }))
+    }));
 
     var label = box.insert(new Q.UI.Text({
         x: 10,
         y: -10 - button.p.h,
         label: stage.options.label
     }));
+
+    Q.audio.play("../audio/MLGHornsSoundEffect.mp3");
 
     button.on("click", function() {
         Q.clearStages();
